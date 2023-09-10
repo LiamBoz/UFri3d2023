bdb5d30ea569221d3204e736ad6b8b811d541c03
10neb01 <benrodkin04@gmail.com>
2023-09-09 21:24:46 -0400

    Added method to ensure we get a valid placement

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/ri3d/ChoosePath.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.utils.ri3d;
+
+import androidx.annotation.NonNull;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.robot.commands.AutoCameraControl;
+
+import java.util.Date;
+import java.util.function.BooleanSupplier;
+
+public class ChoosePath {
+    public static PropPlacement getPlacement(@NonNull AutoCameraControl cam, Telemetry telemetry, BooleanSupplier isStopRequested) {
+        PropPlacement placement = cam.getPlacement();
+        if(placement.name().equals(PropPlacement.UNKNOWN.name())) {
+            telemetry.addData("choosing at", new Date().getTime());
+            cam.periodic();
+            if(isStopRequested.getAsBoolean()) {
+                cam.getCamera().stop();
+                return null;
+            }
+            return getPlacement(cam, telemetry, isStopRequested);
+        }
+
+        return placement;
+    }
+}
